8c29d8944726d6d70efdb9e88a299a7b02beba57
iqqnl <danil.kostuk.06@gmail.com>
2024-12-02 22:02:05 +0300

    fix task 2 and task 1

--- task1/Task1-0.cpp
+++ task1/Task1-0.cpp
@@ -1,64 +1,38 @@
-#include <iostream> 
-#include <cstring> 
-#include <Windows.h>
+#include <iostream>
+#include <cstring>
+using namespace std;
 
-using namespace std; 
- 
-const double xa = 0, xb = 3, xc = 0; 
-const double ya = 0, yb = 0, yc = 4; 
- 
-double getSquareOfTriangle(double x1, double y1, double x2, double y2, double x3, double y3) { 
-    return abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2)) / 2.0; 
-} 
- 
-const double squareABC = getSquareOfTriangle(xa, ya, xb, yb, xc, yc); 
- 
-bool isInTriangle(double x, double y) { 
-    double S1 = getSquareOfTriangle(xa, ya, xb, yb, x, y) ; 
-    double S2 = getSquareOfTriangle(xb, yb, xc, yc, x, y); 
-    double S3 = getSquareOfTriangle(xa, ya, xc, yc, x, y); 
- 
-    return (S1 + S2 + S3) == squareABC; 
-} 
- 
-int main(int argc, char* argv[]) 
-{ 
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "ru_RU");
 
-    bool isHuman = false; 
-    if (argc <= 1 || strcmp(argv[1], "false") != 0) isHuman = true; 
-    if(isHuman) cout << "Введите 0 0 , чтобы завершить ввод" << endl; 
-     
-    double x = 1, y = 1; 
-    while (x != 0 || y != 0) { 
-        if(isHuman) cout << "Введите координаты" << endl; 
-        cin >> x >> y; 
-        if (x == 0 && y == 0) 
-        {
-            break;
+    bool isHuman = false;
+    if (argc <= 1 || strcmp(argv[1], "false") != 0) {
+        isHuman = true;
+    }
+
+    int num, sum = 0, count = 0;
+
+    if (isHuman) {
+        cout << "Введите целые числа (введите 0 для завершения ввода):" << endl;
+    }
+
+    while (cin >> num && num != 0) {
+        sum += num;
+        count++;
+    }
+
+    if (count == 0) {
+        if (isHuman) {
+            cout << "Последовательность не введена." << endl;
         }
-        if (isInTriangle(x, y)) {
-            if(isHuman) 
-            { 
-                cout << "Точка входит" << endl; 
-            }  
-            else 
-            { 
-               cout << "YES" << endl; 
-            }
-        } 
-        else{
-            if(isHuman) 
-            { 
-                cout << "Точка не входит" << endl; 
-            }  
-            else 
-            { 
-                cout << "NO" << endl; 
-            } 
+    } else {
+        double average = static_cast<double>(sum) / count;
+        if (isHuman) {
+            cout << "Среднее арифметическое: " << average << endl;
+        } else {
+            cout << average << endl;
         }
-    } 
- 
-    return 0; 
+    }
+
+    return 0;
 }
--- task2/task2.cpp
+++ task2/task2.cpp
@@ -1,71 +1,75 @@
-#include <iostream> 
-#include <cmath> 
-#include <iomanip> 
-#include <cstring> 
-using namespace std;  
- 
-bool isPrime(int num) { 
-    if (num <= 1) return false; 
-    if (num <= 3) return true; 
-    if (num % 2 == 0 || num % 3 == 0) return false; 
-    for (int i = 5; i * i <= num; i += 6) { 
-        if (num % i == 0 || num % (i + 2) == 0) return false; 
-    } 
-    return true; 
-} 
- 
-bool isPowerOfTwo(int num) { 
-    return num && !(num & (num - 1)); 
-} 
- 
-int main(int argc, char* argv[]) { 
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "ru_RU");
 
-    bool isHuman = false; 
-    if (argc <= 1 || strcmp(argv[1], "false") != 0) { 
-        isHuman = true; 
-    } 
- 
-    int N; 
-    if (isHuman) { 
-        cout << "Введите количество элементов в последовательности: "; 
-    } 
-    cin >> N; 
- 
-    int num, sum = 0, maxNum = INT_MIN, minNum = INT_MAX; 
-    int divisibleByFive = 0, powerOfTwoCount = 0, exceedsPrevSum = 0; 
-    int prev1 = 0, prev2 = 0; 
- 
-    if (isHuman) { 
-        cout << N << " Положительные целые числа:" << endl; 
-    } 
- 
-    for (int i = 0; i < N; ++i) { 
-        cin >> num; 
-        sum += num; 
-        if (num > maxNum) maxNum = num; 
-        if (num < minNum) minNum = num; 
-        if(isPrime(num)){
-            cout << num << endl;
-            if (isHuman) { 
-                cout << " Является простым числом" << endl; 
-            } 
+    bool isHuman = false;
+    if (argc <= 1 || strcmp(argv[1], "false") != 0) {
+        isHuman = true;
+    }
+
+    int N;
+    if (isHuman) {
+        cout << "Введите количество элементов в возрастающей последовательности: ";
+    }
+    cin >> N;
+
+    int num;
+    int prev = INT_MIN;
+    bool isIncreasing = true; 
+
+    if (isHuman) {
+        cout << "Введите элементы " << endl;
+    }
+
+
+    for (int i = 0; i < N; ++i) {
+        cin >> num;
+        if (num <= prev) {
+            isIncreasing = false;
         }
-        if (num % 5 == 0) ++divisibleByFive; 
-        if (isPowerOfTwo(num)) ++powerOfTwoCount; 
-        if (i >= 2 && num > prev1 + prev2) ++exceedsPrevSum; 
- 
-        prev2 = prev1; 
-        prev1 = num; 
-    } 
- 
-    double average = static_cast<double>(sum) / N; 
- 
-    cout << fixed << setprecision(2) << average << endl; 
-    cout << maxNum - minNum << endl; 
-    cout << divisibleByFive << endl; 
-    cout << powerOfTwoCount << endl; 
-    cout << exceedsPrevSum << endl; 
- 
-    return 0; 
+        prev = num;
+    }
+
+    if (!isIncreasing) {
+        cout << "Последовательность не является возрастающей" << endl;
+        return 0;
+    }
+
+
+    int b;
+    if (isHuman) {
+        cout << "Введите число b ";
+    }
+    cin >> b;
+
+    cout << "Новая последовательность: ";
+
+    bool inserted = false;
+    prev = INT_MIN; 
+
+    int i = 0;
+    while ( i < N)  {
+        cin >> num;
+
+        if (!inserted && b <= num) {
+            cout << b << " ";
+            inserted = true;
+        }
+        cout << num << " ";
+        prev = num;
+        ++i;
+    }
+
+
+    if (!inserted) {
+        cout << b;
+    }
+
+    cout << endl;
+    return 0;
 }
